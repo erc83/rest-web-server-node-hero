8290420c674b752c6e0feae49aa44a284a834af1
erc83 <espinoza.eric@gmail.com>
2024-08-10 01:48:40 -0400

    todocontroller.getTodos

--- src/presentation/routes.ts
+++ src/presentation/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { TodosController } from './todos/controller';
 
 
 export class AppRoutes {
@@ -6,15 +7,12 @@ export class AppRoutes {
     static get routes(): Router {
 
         const router = Router();
+        const todoController = new TodosController();
 
-        router.get('/api/todos', (req, res)=> {
 
-            res.json([
-                { id: 1, text: 'Buy milk', createdAt: new Date() },
-                { id: 2, text: 'Buy bread', createdAt: null },
-                { id: 3, text: 'Buy butter', createdAt: new Date() },
-            ])
-        })
+        //router.get('/api/todos', (req, res)=> todoController.getTodos(req, res) );
+        // podemos mandar la referencia a la funcion
+        router.get('/api/todos', todoController.getTodos );
 
 
         return router;
