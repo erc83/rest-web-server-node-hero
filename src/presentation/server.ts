ddd0d6e14a0437c095c0767234d025c301624070
erc83 <espinoza.eric@gmail.com>
2024-08-10 01:35:13 -0400

    implement routes

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,8 +1,9 @@
-import express from 'express';
+import express, { Router } from 'express';
 import path from 'path';
 
 interface Options {
     port: number;
+    routes: Router;
     public_path: string;
 }
 
@@ -10,11 +11,13 @@ export class Server {
     private app = express();
     private readonly port: number;
     private readonly publicPath: string;
-   
+    private readonly routes: Router;
+
     constructor(options:Options) {
-        const { port, public_path = 'public'} = options;
+        const { port, routes, public_path = 'public'} = options;
         this.port = port;
         this.publicPath = public_path;
+        this.routes = routes;
     }
 
     async start() {
@@ -24,7 +27,21 @@ export class Server {
         //* Public Folder
         //this.app.use( express.static( 'public' ) );
         this.app.use( express.static( this.publicPath ) );
-        
+
+        //* Routes
+        this.app.use( this.routes );
+
+        /* this.app.get('/api/todos', (req, res)=> {
+            res.json([
+                { id: 1, text: 'Buy milk', createdAt: new Date() },
+                { id: 2, text: 'Buy bread', createdAt: null },
+                { id: 3, text: 'Buy butter', createdAt: new Date() },
+            ])
+        }) */
+
+
+
+        //* SPA ayuda al router 
         this.app.get('*', (req,  res)=> {
             const indexPath = path.join(__dirname + `../../../${ this.publicPath }/index.html`)
             res.sendFile(indexPath);
