8fe3438f8ed6a360f3964d5898591ca85737bdd9
erc83 <espinoza.eric@gmail.com>
2024-09-21 02:53:24 -0300

    crud ok prisma en controllador

--- src/presentation/todos/controller.ts
+++ src/presentation/todos/controller.ts
@@ -1,12 +1,5 @@
-import { create } from "domain";
 import { Request, Response } from "express"
 import { prisma } from "../../data/postgres";
-
-const todos = [
-    { id: 1, text: 'Buy milk', createdAt: new Date() },
-    { id: 2, text: 'Buy bread', createdAt: null },
-    { id: 3, text: 'Buy butter', createdAt: new Date() },
-];
 
 
 export class TodosController {
@@ -14,14 +7,21 @@ export class TodosController {
     //* DI
     constructor() {}
 
-        public getTodos = (req: Request, res: Response)=> {
+        public getTodos = async (req: Request, res: Response)=> {
+            const todos = await prisma.todo.findMany()
             res.json(todos);
         }
 
-        public getTodoById = (req: Request, res: Response)=> {
+        public getTodoById = async (req: Request, res: Response)=> {
             const id = +req.params.id;
             if( isNaN(id) ) return res.status(400).json({ error: 'ID argument is not a number' });
-            const todo = todos.find( todo => todo.id === id );
+            
+            const todo = await prisma.todo.findUnique({
+                where: {
+                    int: id
+                }
+            });           
+            
             ( todo )
                 ? res.json(todo)
                 : res.status(404).json({ error: `TODO with id ${ id } not found`})
@@ -40,33 +40,46 @@ export class TodosController {
 
         };
 
-        public updateTodo = (req: Request, res: Response) => {
+        public updateTodo = async (req: Request, res: Response) => {
             const id = +req.params.id; // viene con un texto se convierte con el +
             if( isNaN(id) ) return res.status( 400 ).json( { error: 'Id argument is not a number' } );
 
-            const todo = todos.find( todo => todo.id === id );
+            const todo = await prisma.todo.findFirst({
+                where : { int: id }
+            })
             if( !todo ) return res.status( 404 ).json( { error: `Todo with id ${ id } not found` });
-
-            const { text, createdAt } = req.body;
-            //if( !text ) return res.status( 400 ).json( {error: 'Text property is required' });
-
-            todo.text = text || todo.text; // va a ser el texto si viene si no va a ser el mismo valor
-            ( createdAt === 'null')
-                ? todo.createdAt = null
-                : todo.createdAt = new Date( createdAt || todo.createdAt );
-
-            res.json( todo )
+            
+            const { text, completedAt } = req.body;
+            const updateTodo = await prisma.todo.update({
+                where: {int : id },
+                data: { 
+                    text, 
+                    completedAt: (completedAt) ? new Date(completedAt) : null 
+                }
+            })
+            
+            res.json( updateTodo )
         }
 
-        public deleteTodo = (req: Request, res: Response ) => {
+        public deleteTodo = async (req: Request, res: Response ) => {
             const id = +req.params.id; // viene con un texto se convierte con el +
             if( isNaN(id) ) return res.status( 400 ).json( { error: 'Id argument is not a number' } );
 
-            const todo = todos.find(todo => todo.id === id );
+            const todo = await prisma.todo.findUnique({
+                where: {
+                    int: id
+                }
+            });  
+
             if( !todo ) return res.status( 404 ).json({ error : `Todo with id ${ id } not found`})
 
-            todos.splice( todos.indexOf( todo ), 1 ); // tambien se puede realizar con un filter
+            const deleted = await prisma.todo.delete({
+                where : { int : id }
+            });
+            
+            ( deleted )
+                ? res.json( deleted )
+                : res.status(400).json({ error: `Todo with id ${ id } not found `})
 
-            res.json( todo );
         }
 }
