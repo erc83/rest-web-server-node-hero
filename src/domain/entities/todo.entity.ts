d9bea35d21c14e08e4c13cd784843ba40ec0afac
erc83 <espinoza.eric@gmail.com>
2024-09-21 15:43:18 -0300

    TodoDatasource implementacion getAll

--- src/domain/entities/todo.entity.ts
+++ src/domain/entities/todo.entity.ts
@@ -11,5 +11,22 @@ export class TodoEntity {
         return !!this.completedAt;      // Doble negacion si tien un valor regresa true
     }
 
+    //metodo estatico fromObject, la mando el objeto y crea la entidad
+    public static fromObject( object: {[key: string]: any} ) : TodoEntity{      // TodoEntity valor de retorno
+        const { id, text, completedAt } = object;
+        if( !id ) throw 'Id is required'
+        if( !text ) throw 'text is required'
+        
+        let newCompletedAt;
+
+        if( completedAt ) {
+            newCompletedAt = new Date(completedAt);
+            if( isNaN( newCompletedAt.getTime() )) {
+                throw 'CompletedAt is not a valid date'
+            }
+        }
+
+        return new TodoEntity( id, text, completedAt )
+    }
 }
 
