6b7e792820a1dbcf5cea3cce41e7cac76779ee33
erc83 <espinoza.eric@gmail.com>
2024-09-22 17:41:11 -0300

    testing app.test.ts

--- tests/app.test.ts
+++ tests/app.test.ts
@@ -1,9 +1,33 @@
 
+import { envs } from '../src/config/envs'
+import { AppRoutes } from '../src/presentation/routes'
+import { Server } from '../src/presentation/server'
 
-describe('Testing App.ts', () => {
+// todo lo que esta en esta ruta es un mock 
+jest.mock('../src/presentation/server')
 
-    test('should work', () => { 
-        expect(true).toBeTruthy();
+describe('should call server with arguments and start', () => {
+
+    test('should work', async () => { 
+        
+        await import('../src/app')
+
+        expect(Server).toHaveBeenCalledTimes(1);
+
+        // evaluamos que sea llamado con tales argumentos
+        expect( Server ).toHaveBeenCalledWith({
+            port: envs.PORT,
+            public_path: envs.PUBLIC_PATH,
+            //routes: AppRoutes.routes       //esta llamando a dos lugares diferentes por eso no usamos el AppRoutes
+            routes: expect.any(Function),
+        })
+
+        // esperamos que el server.start() ha sido llamado
+        //expect(Server.prototype.start).toHaveBeenCalledWith({
+        //    a:1
+        //})
+        expect(Server.prototype.start).toHaveBeenCalledWith()
+
     })
 
 })
