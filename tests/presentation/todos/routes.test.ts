d287eb75b1089f9fee0f02c84fe40fb173083379
erc83 <espinoza.eric@gmail.com>
2024-09-22 22:51:23 -0300

    get todos con busqueda en la database testing

--- tests/presentation/todos/routes.test.ts
+++ tests/presentation/todos/routes.test.ts
@@ -1,25 +1,42 @@
 import request from 'supertest';
 import { testServer } from '../../test-server'
-import { after } from 'node:test';
+import { prisma } from '../../../src/data/postgres';
 
 describe('TODO route testing', () => {
 
     //levantando el servidor
     beforeAll(async() => {          // Antes de todas las pruebas
         await  testServer.start();
-    })
+    }) 
 
     afterAll(() => {                // despues de todas las pruebas
         testServer.close()
     })
 
+    const todo1 = { text: 'Hola mundo test 1' };
+    const todo2 = { text: 'Hola mundo test 2' };
+
+
     test('should return TODOs api/todos', async () => { 
+        //cuando se ejecute esta prueba comience de 0 las inserciones
+        await prisma.todo.deleteMany();
+
+        await prisma.todo.createMany({
+            data: [ todo1, todo2 ]
+        })
+
         // nos pide la aplicacion que es de nuestro server
         const response = await request( testServer.app )
             .get('/api/todos')
             .expect(200);               //  -> nos muestra el error que no tenemos la tabla creada
 
-        console.log(response.body);
+        //console.log(response.body);
+        expect( response.body ).toBeInstanceOf( Array );
+        expect( response.body.length ).toBe(2);
+        expect( response.body[0].text).toBe( todo1.text )
+        expect( response.body[1].text).toBe( todo2.text )
+
+        expect( response.body[0].completedAt).toBeNull();
     })
 
 })
